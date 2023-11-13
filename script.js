003ea152b449e5d8f0671b677599f4eaac20b543
jsneath <jamessneath1@gmail.com>
2023-11-13 18:34:41 +0000

    save and get text to local storage

--- script.js
+++ script.js
@@ -13,18 +13,33 @@ $("#currentDay").text(todaysDate);
 var now = dayjs()
 var currentHour = now.hour()
 
+
+function loadSavedData() {
+    document.querySelectorAll('.row.time-block').forEach(function(timeBlock) {
+        var id = timeBlock.id; 
+        var savedData = localStorage.getItem(id);
+        if (savedData !== null) {
+            var textarea = timeBlock.querySelector('.description');
+            textarea.value = savedData;
+        }
+    });
+}
+
+// Call the function to load saved data
+loadSavedData();
+
 // if time on schedule is > current time, make green
 
 // if time on schedule is < current time, malre grey
 // if time on shedule is = current time, make red
 document.querySelectorAll('.row.time-block').forEach(function(timeBlock) {
-    // Extract the time from the .hour div
+    
     var timeString = timeBlock.querySelector('.hour').textContent;
     console.log("this is timestring" + timeString)
     var blockHour = parseInt(timeString.split(':')[0], 10);
     console.log("this is blockHour" + blockHour)
 
-    // Compare and apply classes
+ 
     if (blockHour < currentHour) {
         timeBlock.classList.add('past');
     } else if (blockHour === currentHour) {
@@ -32,5 +47,20 @@ document.querySelectorAll('.row.time-block').forEach(function(timeBlock) {
     } else {
         timeBlock.classList.add('future');
     }
+
+ 
 });
 
+
+document.querySelectorAll('.saveBtn').forEach(function(saveButton) {
+    saveButton.addEventListener('click', function() {
+        
+        var timeBlock = saveButton.parentElement;
+        var id = timeBlock.id; 
+
+        var textarea = timeBlock.querySelector('.description');
+
+        localStorage.setItem(id, textarea.value);
+        // show saved somewhere
+    });
+});
